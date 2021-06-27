9ce5ff00f5a9a9ca4669e8f779d0705d820aa335
mrverdinsky <goncalo.vila.verde@hotmail.com>
2021-06-27 16:17:44 +0100

    Add Cell Script

--- Cell.cs
+++ Cell.cs
@@ -2,9 +2,31 @@
 
 namespace MVC_Conway.Common
 {
+    /// <summary>
+    /// Represents every cell in the simulation
+    /// </summary>
     public class Cell
     {
-        private int pos;
-        //test
+        /// <summary>
+        /// Prperty used to save the cell's position
+        /// </summary>
+        public Position CellPos { get; private set;}
+
+        /// <summary>
+        /// Property used to save the cell's type
+        /// </summary>
+        public CellType CellType { get; private set;}
+
+        /// <summary>
+        /// Constructor for the Cell Class
+        /// </summary>
+        /// <param name="position">Cell's position Value</param>
+        /// <param name="cellType">Cell's Type Value</param>
+        public Cell(Position position, CellType cellType)
+        {
+            CellPos = position;
+            CellType = cellType;
+        }
+
     }
 }
