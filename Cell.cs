b29092d6431e6ae0abd1c8805932b826245f3797
mrverdinsky <goncalo.vila.verde@hotmail.com>
2021-06-29 19:51:36 +0100

    Add TypeGenerator method to Cell.cs and GridSetup method to Grid.cs

--- Cell.cs
+++ Cell.cs
@@ -1,4 +1,6 @@
 ﻿
+using System;
+
 namespace MVC_Conway.Common
 {
     /// <summary>
@@ -6,26 +8,52 @@ namespace MVC_Conway.Common
     /// </summary>
     public class Cell
     {
+
         /// <summary>
         /// Prperty used to save the cell's position
         /// </summary>
-        public Position CellPos { get; private set;}
+        public Position CellPos { get; private set; }
 
         /// <summary>
         /// Property used to save the cell's type
         /// </summary>
-        public CellType CellType { get; private set;}
+        public CellType CellType { get; private set; }
 
+        private Random rng;
         /// <summary>
         /// Constructor for the Cell Class
         /// </summary>
         /// <param name="position">Cell's position Value</param>
         /// <param name="cellType">Cell's Type Value</param>
-        public Cell(Position position, CellType cellType)
+        public Cell(Position position)
         {
             CellPos = position;
-            CellType = cellType;
+            CellType = TypeGenerator();
         }
+
+        private CellType TypeGenerator()
+        {
+            int cellTypeValue;
+            rng = new Random();
+            cellTypeValue = rng.Next(1, 3);
+
+            switch (cellTypeValue)
+            {
+                case 1:
+                    return CellType.Rock;
+                case 2:
+                    return CellType.Paper;
+
+                case 3:
+                    return CellType.Scissors;
+
+                default:
+                    return CellType.Empty;     
+            }
+
+        }
+
+
 
     }
 }
--- Grid.cs
+++ Grid.cs
@@ -13,7 +13,20 @@ namespace MVC_Conway.Common
         /// Property used to save all the cells
         /// in the simulation
         /// </summary>
-        public Cell[,] CellGroup { get; private set;}
+        public ICollection<Cell> CellGroup { get; private set;}
+
+        /// <summary>
+        /// Property used to acess the maximum
+        /// dimensions of the grid's rows
+        /// </summary>
+        public byte MaxRows { get; private set;}
+
+        /// <summary>
+        /// Property used to acess the maximum
+        /// dimensions of the grid's collumns
+        /// </summary>
+        public byte MaxColumn { get; private set; }
+
 
 
         /// <summary>
@@ -21,9 +34,24 @@ namespace MVC_Conway.Common
         /// </summary>
         /// <param name="cellGroup">Collection of
         /// cell's in the simulation</param>
-        public Grid(Cell[,] cellGroup) 
+        public Grid(byte maxRow, byte maxCollumn) 
         {
-            CellGroup = cellGroup;
+           
+            MaxRows = maxRow;
+            MaxColumn = maxCollumn;
+        }
+
+        public void GridSetup()
+        {
+            Cell tempCell;
+            for (byte i = 0; i < MaxRows; i++)
+            {
+                for (byte j = 0; j < MaxColumn; j++)
+                {
+                    tempCell = new Cell(new Position(i, j));
+                    CellGroup.Add(tempCell);
+                }
+            }
         }
     }
 }
