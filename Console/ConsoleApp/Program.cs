169ac6ae3e90f373d43ea58966f855cf9dadbe65
Miguel-Martinho <miguelpm2001@gmail.com>
2021-07-01 10:42:01 +0100

    Update common

--- Console/ConsoleApp/Program.cs
+++ Console/ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 using MVC_Conway.Common;
 
 namespace ConsoleApp
@@ -7,8 +8,12 @@ namespace ConsoleApp
     {
         private static void Main(string[] args)
         {
-            Cell cell = new Cell();
-            Console.WriteLine("Hello World!");
+            Parameters parameters = new Parameters(
+                Convert.ToInt16(args[0]), 
+                Convert.ToInt16(args[1]), 
+                float.Parse(args[2], CultureInfo.InvariantCulture),
+                float.Parse(args[3], CultureInfo.InvariantCulture), 
+                float.Parse(args[4], CultureInfo.InvariantCulture));
         }
     }
 }
